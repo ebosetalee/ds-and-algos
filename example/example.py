c65d338e1afc6dd47ce51860020f6fdd884183a2
Raymond Tukpe <rtukpe@gmail.com>
2020-08-22 11:16:08 +0100

    feat: adds example module to introduce concept of testing

---
+++ example/example.py
@@ -0,0 +1,16 @@
+def add(first, second):
+    """
+    :param first: an integer
+    :param second: an integer
+    :returns: The sum of the two numbers
+    """
+    pass
+
+
+def power(number, exponent):
+    """
+    :param number: a positive integer
+    :param exponent: a positive integer
+    :returns: The power of the number to the exponent
+    """
+    pass
---
+++ example/test/example_test.py
@@ -0,0 +1,18 @@
+import unittest
+from example.example import add, power
+
+
+class TestExample(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(1, 2), 3)
+
+    def test_add_two_arguments_must_be_supplied(self):
+        with self.assertRaises(TypeError):
+            add(1)
+
+    def test_power(self):
+        self.assertEqual(power(3, 2), 9)
+
+    def test_power_number_should_not_be_negative(self):
+        with self.assertRaises(TypeError):
+            power(-1, 2)
