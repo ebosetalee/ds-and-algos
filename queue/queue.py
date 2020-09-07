5858f88f9516799ac98d6f0e8379602e540dde10
Raymond Tukpe <rtukpe@gmail.com>
2020-09-07 12:19:04 +0100

    feat: adds queue tests

---
+++ queue/queue.py
@@ -0,0 +1,39 @@
+from linked_list.linked_list import LinkedList
+
+
+class Queue:
+    """
+    The queue uses a LinkedList internally to manage the items
+    """
+
+    def __init__(self):
+        """
+        Initialize the Queue with an empty LinkedList
+        """
+        pass
+
+    def is_empty(self):
+        """
+        :returns: True if the LinkedList is empty and False if it isn't
+        """
+        pass
+
+    def enqueue(self, value):
+        """
+        Adds the value to the back of the LinkedList
+        :param value: the value to be added
+        """
+        pass
+
+    def dequeue(self):
+        """
+        Removes the value at the head
+        :return: the value of the item at the head
+        """
+        pass
+
+    def peek(self):
+        """
+        :returns: the value of the item at the head
+        """
+        pass
