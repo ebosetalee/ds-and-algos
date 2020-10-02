2a5ddeb474f965a92e739a19eefa487f6931a33b
ebosetalee <princess01ella.miley@gmail.com>
2020-10-02 13:20:56 +0100

    more tests including collisions

--- hash_table/test/hash_table_test.py
+++ hash_table/test/hash_table_test.py
@@ -5,7 +5,7 @@ from hash_table.hash_table import HashTable
 class MyTestCase(unittest.TestCase):
     def test_empty_hash_table(self):
         hash_table = HashTable()
-        self.assertIsNone(hash_table.dict)
+        self.assertIsNone(hash_table.get_keys())
 
     def test_hash(self):
         hash_table = HashTable()
@@ -19,7 +19,16 @@ class MyTestCase(unittest.TestCase):
         hash_table = HashTable()
         value = "Jasmine Smith"
         hash_table.set(value)
-        self.assertEqual(hash_table.dict, {260: "Jasmine Smith"})
+        self.assertEqual(hash_table.get("Jasmine Smith"), 260)
+
+    def get_in_dict(self):
+        pass
+
+    def test_set_empty_dict(self):
+        pass
+
+    def test_set_not_empty_dict(self):
+        pass
 
     def test_set_collision(self):
         pass
@@ -29,10 +38,26 @@ class MyTestCase(unittest.TestCase):
         hash_table.set("Jasmine Smith")
         self.assertEqual(hash_table.get_keys(), {260: "Jasmine Smith"})
 
-    def test_has(self):
+    def test_get_keys_empty_dict(self):
+        hash_table = HashTable()
+        self.assertIsNone(hash_table.get_keys())
+
+    def test_has_in_dict(self):
         pass
 
-    def test_delete(self):
+    def test_has_not_in_dict(self):
+        pass
+
+    def test_has_collision(self):
+        pass
+
+    def test_delete_not_in_list(self):
+        pass
+
+    def test_delete_in_list(self):
+        pass
+
+    def test_delete_collision_keys(self):
         pass
 
 
