41fd3e8be73b5bd4c548f0be0099d8d9628348c6
ebosetalee <princess01ella.miley@gmail.com>
2020-10-04 02:45:00 +0100

    rewrote tests

--- hash_table/test/hash_table_test.py
+++ hash_table/test/hash_table_test.py
@@ -13,7 +13,9 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(hash_table.hash(value), 260)
 
     def test_get_not_in_hash_table(self):
-        pass
+        hash_table = HashTable()
+        value = "Jasmine Smith"
+        self.assertIsNone(hash_table.get(value), None)
 
     def test_set(self):
         hash_table = HashTable()
@@ -22,15 +24,26 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(hash_table.get("Jasmine Smith"), 260)
 
     def get_in_hash_table(self):
-        pass
+        hash_table = HashTable()
+        value = "Jasmine Smith"
+        self.assertEqual(hash_table.get(value), 260)
 
     def test_set_empty_hash_table(self):
-        pass
+        hash_table = HashTable()
+        value = "Jasmine Smith"
+        hash_table.set(value)
+        self.assertEqual(hash_table.get_keys()[0], 260)
 
     def test_set_not_empty_hash_table(self):
-        pass
+        hash_table = HashTable()
+        hash_table.set("Thomas Job")
+        hash_table.set("Jasmine Smith")
+        self.assertNotEqual(hash_table.get_keys()[0], 260)
 
     def test_set_collision_occurred(self):
+        # hash_table = HashTable()
+        # hash_table.set("Jasmine Smith")
+        # hash_table.set("Jasie Smminth")
         pass
 
     def test_get_keys(self):
@@ -43,19 +56,32 @@ class MyTestCase(unittest.TestCase):
         self.assertIsNone(hash_table.get_keys())
 
     def test_has_in_hash_table(self):
-        pass
+        hash_table = HashTable()
+        hash_table.set("Jasmine Smith")
+        hash_table.set("Thomas Job")
+        self.assertEqual(hash_table.has("Jasie Smminth"), True)
 
     def test_has_not_in_hash_table(self):
-        pass
+        hash_table = HashTable()
+        hash_table.set("Jane Francis")
+        hash_table.set("Thomas Job")
+        self.assertEqual(hash_table.has("Jasmine Smith"), False)
 
     def test_has_collision(self):
         pass
 
     def test_delete_not_in_hash_table(self):
-        pass
+        hash_table = HashTable()
+        hash_table.set("Jane Francis")
+        hash_table.set("Thomas Job")
+        self.assertEqual(hash_table.delete("Jasmine Smith"), False)
 
     def test_delete_in_hash_table(self):
-        pass
+        hash_table = HashTable()
+        hash_table.set("Jane Francis")
+        hash_table.set("Jasmine Smith")
+        hash_table.set("Thomas Job")
+        self.assertEqual(hash_table.delete("Jasmine Smith"), True)
 
     def test_delete_collision_keys(self):
         pass
