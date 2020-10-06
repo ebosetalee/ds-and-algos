67752276e51e5bf6740f9c1206767e14a284f3db
ebosetalee <princess01ella.miley@gmail.com>
2020-10-06 11:10:45 +0100

    rewrote tests

--- hash_table/test/hash_table_test.py
+++ hash_table/test/hash_table_test.py
@@ -5,7 +5,7 @@ from hash_table.hash_table import HashTable
 class MyTestCase(unittest.TestCase):
     def test_empty_hash_table(self):
         hash_table = HashTable()
-        self.assertIsNone(hash_table.get_keys())
+        self.assertEqual(hash_table.get_keys(), [])
 
     def test_hash(self):
         hash_table = HashTable()
@@ -21,51 +21,37 @@ class MyTestCase(unittest.TestCase):
         hash_table = HashTable()
         value = "Jasmine Smith"
         hash_table.set(value)
-        self.assertEqual(hash_table.get("Jasmine Smith"), 260)
+        self.assertEqual(hash_table.get_keys(), ["Jasmine Smith"])
 
-    def get_in_hash_table(self):
-        hash_table = HashTable()
-        value = "Jasmine Smith"
-        self.assertEqual(hash_table.get(value), 260)
-
-    def test_set_empty_hash_table(self):
+    def test_get_in_hash_table(self):
         hash_table = HashTable()
         value = "Jasmine Smith"
         hash_table.set(value)
-        self.assertEqual(hash_table.get_keys()[0], 260)
-
-    def test_set_not_empty_hash_table(self):
-        hash_table = HashTable()
-        hash_table.set("Thomas Job")
-        hash_table.set("Jasmine Smith")
-        self.assertNotEqual(hash_table.get_keys()[0], 260)
+        self.assertEqual(hash_table.get(value), 260)
 
     def test_set_collision_occurred(self):
-        # hash_table = HashTable()
-        # hash_table.set("Jasmine Smith")
-        # hash_table.set("Jasie Smminth")
-        pass
+        hash_table = HashTable()
+        hash_table.set("Jasmine Smith")
+        hash_table.set("Jasie Smminth")
+        self.assertEqual(hash_table.get("Jasmine Smith"), 260)
+        self.assertNotEqual(hash_table.get("Jasie Smminth"), hash_table.table[260])
 
     def test_get_keys(self):
         hash_table = HashTable()
         hash_table.set("Jasmine Smith")
-        self.assertEqual(hash_table.get_keys(), {260: "Jasmine Smith"})
-
-    def test_get_keys_empty_hash_table(self):
-        hash_table = HashTable()
-        self.assertIsNone(hash_table.get_keys())
+        self.assertEqual(hash_table.get_keys(), ["Jasmine Smith"])
 
     def test_has_in_hash_table(self):
         hash_table = HashTable()
         hash_table.set("Jasmine Smith")
         hash_table.set("Thomas Job")
-        self.assertEqual(hash_table.has("Jasie Smminth"), True)
+        self.assertEqual(hash_table.has(260), True)
 
     def test_has_not_in_hash_table(self):
         hash_table = HashTable()
         hash_table.set("Jane Francis")
         hash_table.set("Thomas Job")
-        self.assertEqual(hash_table.has("Jasmine Smith"), False)
+        self.assertEqual(hash_table.has(260), False)
 
     def test_has_collision(self):
         pass
