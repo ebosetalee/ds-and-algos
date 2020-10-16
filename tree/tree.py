87ddb569b6fcbb7f65c8e5e4e6a1aad408fc2e71
ebosetalee <princess01ella.miley@gmail.com>
2020-10-16 12:28:48 +0100

    Created files and folders

---
+++ tree/tree.py
@@ -0,0 +1,10 @@
+from treenode.treenode import TreeNode
+
+
+class Tree:
+    def __init__(self, value):
+        self.value = value
+        self.parent = None
+
+    def root_node(self, value):
+        pass
