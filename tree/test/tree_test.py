0c2ae9fe818602c4095a8d97962224b306135d97
ebosetalee <princess01ella.miley@gmail.com>
2020-10-21 15:22:12 +0100

    Wrote delete function and tests

--- tree/test/tree_test.py
+++ tree/test/tree_test.py
@@ -78,6 +78,68 @@ class MyTestCase(unittest.TestCase):
         tree.add_child(6)
         self.assertEqual(tree.get_parent(7), 5)
 
+    def test_delete_root_node(self):
+        tree = Tree()
+        tree.add_child(5)
+        tree.add_child(4)
+        tree.add_child(6)
+        tree.delete(5)
+        self.assertEqual(tree.display(), [6, 4])
+
+    def test_delete_node_without_child(self):
+        tree = Tree()
+        tree.add_child(5)
+        tree.add_child(1)
+        tree.add_child(4)
+        tree.add_child(7)
+        tree.add_child(9)
+        tree.add_child(6)
+        tree.add_child(8)
+        self.assertEqual(tree.display(), [5, 1, 4, 7, 6, 9, 8])
+        tree.delete(4)
+        self.assertEqual(tree.display(), [5, 1, 7, 6, 9, 8])
+        tree.delete(8)
+        self.assertEqual(tree.display(), [5, 1, 7, 6, 9])
+
+    def test_delete_node_with_one_child(self):
+        tree = Tree()
+        tree.add_child(5)
+        tree.add_child(1)
+        tree.add_child(4)
+        tree.add_child(7)
+        tree.add_child(9)
+        tree.add_child(6)
+        tree.add_child(8)
+        self.assertEqual(tree.display(), [5, 1, 4, 7, 6, 9, 8])
+        tree.delete(9)
+        self.assertEqual(tree.display(), [5, 1, 4, 7, 6, 8])
+        tree.delete(1)
+        self.assertEqual(tree.display(), [5, 4, 7, 6, 8])
+
+    def test_delete_node_with_children(self):
+        tree = Tree()
+        tree.add_child(5)
+        tree.add_child(1)
+        tree.add_child(4)
+        tree.add_child(7)
+        tree.add_child(9)
+        tree.add_child(6)
+        tree.add_child(8)
+        self.assertEqual(tree.display(), [5, 1, 4, 7, 6, 9, 8])
+        tree.delete(7)
+        self.assertEqual(tree.display(), [5, 1, 4, 8, 6, 9])
+        tree.delete(8)
+        self.assertEqual(tree.display(), [5, 1, 4, 9, 6])
+
+    def test_delete_not_in_the_tree(self):
+        tree = Tree()
+        tree.add_child(5)
+        tree.add_child(2)
+        tree.add_child(7)
+        self.assertEqual(tree.display(), [5, 2, 7])
+        tree.delete(6)
+        self.assertEqual(tree.display(), [5, 2, 7])
+
 
 if __name__ == '__main__':
     unittest.main()
